1edae2b51128a3df6ba851ab9095c03832ad176b
sarafat27 <sarafat2769@gmail.com>
2022-04-22 20:07:43 +0600

    update form created

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, []);
+
+    const handleUserDelete = id => {
+        const proceed = window.confirm('are you sure to delete this?');
+        if (proceed) {
+            console.log('deleting user', id);
+            const url = `http://localhost:5000/user/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log('deleted');
+                        const remaining = users.filter(user => user._id !== id);
+                        setUsers(remaining)
+                    }
+                })
+        }
+    }
+
     return (
         <div>
-            <h2>Home</h2>
+            <h2>Available users: {users.length}</h2>
+            <ul>
+                {
+                    users.map(user => <li
+                        key={user._id}
+                    >{user.name} : {user.email}
+                        <Link to={`/update/${user._id}`}><button>Update</button></Link>
+                        <button onClick={() => handleUserDelete(user._id)}>X</button>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
